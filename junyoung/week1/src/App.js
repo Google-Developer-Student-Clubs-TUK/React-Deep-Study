ca33ea5b33734da13da43b190c4417805b6c9195
junyoung <no2jfamily@tukorea.ac.kr>
2022-10-19 18:46:30 +0900

    commit practice

--- junyoung/week1/src/App.js
+++ junyoung/week1/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React2
+          Learn React3
         </a>
       </header>
     </div>
