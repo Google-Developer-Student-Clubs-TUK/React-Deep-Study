70fa184f19cc4b6d956769bb2ab266c0a073bec7
jinhobyeong <jhb_0921@naver.com>
2022-10-27 23:54:44 +0900

    [진호병] week3 -1

--- hobyeong/week3/chapter5/src/App.test.tsx
+++ hobyeong/week3/chapter5/src/App.test.tsx
@@ -1,23 +1,27 @@
 /* eslint-disable testing-library/no-node-access */
 /* eslint-disable testing-library/no-container */
+import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 describe("<App />", () => {
-  it("renders component coreactly", () => {
+  it("renders component correctly", () => {
     const { container } = render(<App />);
+
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
+
+    expect(container.getElementsByClassName("App-logo")).toHaveLength(1);
+    expect(container.getElementsByClassName("App-logo")[0]).toHaveAttribute(
+      "src",
+      "logo.svg"
+    );
+
     expect(container.getElementsByTagName("p")).toHaveLength(1);
     expect(container.getElementsByTagName("p")[0]).toHaveTextContent(
       "Edit src/App.js and save to reload."
     );
-    // expect(container).toMatchSnapshot();
+
+    expect(container).toMatchSnapshot();
   });
 });
