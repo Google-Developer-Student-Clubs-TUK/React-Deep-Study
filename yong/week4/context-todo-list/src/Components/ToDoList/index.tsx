d54eea015c2ee3e9aa944247fd521ec6591c969e
pione3r <jhyong0626@gmail.com>
2022-11-07 19:00:05 +0900

    [용준현] week4 실습 완료

---
+++ yong/week4/context-todo-list/src/Components/ToDoList/index.tsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import Styled from 'styled-components';
+
+import { ToDoItem } from 'Components/ToDoItem';
+
+import { ToDoListContext } from 'Contexts';
+
+const Container = Styled.div`
+  min-width: 350px;
+  height: 400px;
+  overflow-y: scroll;
+  border: 1px solid #BDBDBD;
+  margin-bottom: 20px;
+`;
+
+export const ToDoList = () => {
+  const { toDoList, deleteToDo } = useContext(ToDoListContext);
+
+  return (
+    <Container data-testid="toDoList">
+      {toDoList.map((item, index) => (
+        <ToDoItem key={item} label={item} onDelete={() => deleteToDo(index)} />
+      ))}
+    </Container>
+  );
+};
